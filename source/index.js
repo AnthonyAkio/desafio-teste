3c66bf01ff5f60c3253ce889966f8a0eb9e3ed91
AnthonyAkio <anthony.akio2@gmail.com>
2023-10-25 03:32:44 -0300

    minha primeira entrega de projeto

--- source/index.js
+++ source/index.js
@@ -1,2 +1,19 @@
-let testando = 123
-console.log(testando)
+const username = "Akio ";
+let XP = 10700
+if (XP<=1000) {
+    console.log("O Herói de nome " + username + "está no nível de Ferro")
+}else if (XP>=1001 && XP<=2000) {
+    console.log("O Herói de nome " + username + "está no nível de Bronze")
+}else if (XP>=2001 && XP<=6000) {
+    console.log("O Herói de nome " + username + "está no nível de Prata")
+}else if (XP>=6001 && XP<=7000) {
+    console.log("O Herói de nome " + username + "está no nível de Ouro")
+}else if (XP>=7001 && XP<=8000) {
+    console.log("O Herói de nome " + username + "está no nível de Platina")
+}else if (XP>=8001 && XP<=9000) {
+    console.log("O Herói de nome " + username + "está no nível de Ascendente")
+}else if (XP>=9001 && XP<=10000) {
+    console.log("O Herói de nome " + username + "está no nível de Imortal")
+}else if (XP>=10001) {
+    console.log("O Herói de nome " + username + "está no nível de Radiante")
+}
